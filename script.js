488cc24ce1defcd342dcb2cd91c42376d904aa2b
Manonthemon <klimowicz.maciej@gmail.com>
2023-06-05 19:33:29 +0100

    Debugged scrolling not working

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ slider.addEventListener("mousedown", (e) => {
   isDown = true;
   slider.classList.add("active");
   startX = e.pageX - slider.offsetLeft;
-  scrollLeft - slider.scrollLeft;
+  scrollLeft = slider.scrollLeft;
  
 });
 
@@ -22,7 +22,10 @@ slider.addEventListener("mouseup", () => {
   slider.classList.remove("active");
 });
 
-slider.addEventListener("mousemove", () => {
-  if (!isDown) return; //stops from running when mouse not down
-
-});
+slider.addEventListener('mousemove', (e) => {
+    if (!isDown) return;  // stop the fn from running
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    const walk = (x - startX) * 3;
+    slider.scrollLeft = scrollLeft - walk;
+  });
