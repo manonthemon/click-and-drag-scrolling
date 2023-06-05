a0bb563fccf281af90566dc8e29b7c130a168978
Manonthemon <klimowicz.maciej@gmail.com>
2023-06-05 18:54:45 +0100

    Added detecting of mouse click position and scroll position

--- script.js
+++ script.js
@@ -4,20 +4,25 @@ let isDown = false;
 let startX;
 let scrollLeft;
 
-slider.addEventListener("mousedown", () => {
+slider.addEventListener("mousedown", (e) => {
   isDown = true;
+  slider.classList.add("active");
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft - slider.scrollLeft;
+ 
 });
 
 slider.addEventListener("mouseleave", () => {
   isDown = false;
+  slider.classList.remove("active");
 });
 
 slider.addEventListener("mouseup", () => {
   isDown = false;
+  slider.classList.remove("active");
 });
 
 slider.addEventListener("mousemove", () => {
-    if(!isDown) return //stops from running when mouse not down
-    console.log(isDown)
-    console.log('Do work')
+  if (!isDown) return; //stops from running when mouse not down
+
 });
